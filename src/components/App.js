b07970cb11f9f99bafc3e5f709c65ae09faa1078
Tim <50870607+toothless13@users.noreply.github.com>
2023-09-28 14:28:09 +0100

    create new directories and move files appropriately

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
+import "../styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
+        <p>{2 + 2}</p>
       </header>
     </div>
   );
