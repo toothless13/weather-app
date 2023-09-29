63c824c4805fa8d6f455fc180972ecc2b66450c5
Tim <50870607+toothless13@users.noreply.github.com>
2023-09-29 14:34:34 +0100

    Change linter for requiring React

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,9 @@
 import "../styles/App.css";
 import LocationDetails from "./LocationDetails";
 
-const App = ({ location }, { forecasts }) => {
+const App = ({ location }) => {
   const { city, country } = location;
-  console.log(forecasts);
-  // const { date } = forecasts;
-  // console.log(date);
-  // const { date, temperature, description, icon } = forecasts[0];
-  // console.log(date, temperature, description, icon);
+
   return (
     <div className="App">
       <LocationDetails city={city} country={country} />
