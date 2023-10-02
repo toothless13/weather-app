c607442366e7273990a5389f482f0b273dd5b80b
Tim <50870607+toothless13@users.noreply.github.com>
2023-10-02 16:50:00 +0100

    add in functionality to display weather icons

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -1,10 +1,13 @@
+import iconData from "../data/iconData.json";
+
 const ForecastSummary = (props) => {
   const { date, description, icon, temperature } = props;
+  const weatherCode = `${icon.slice(0, 1)}00`;
   return (
     <div className="forecast-summary" data-testid="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
       <div className="forecast-summary__icon" data-testid="forecast-icon">
-        {icon}
+        <img src={iconData[weatherCode]} alt="icon showing weather code" />
       </div>
       <div className="forecast-summary__temperature">
         {temperature.max}&deg;C
