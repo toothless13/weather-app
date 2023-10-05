3bf6bd8de4f2a50befd5295a2e1021edb2eec388
Tim <50870607+toothless13@users.noreply.github.com>
2023-10-05 21:41:28 +0100

    add search functionality to app

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,13 @@ import ForecastDetails from "./ForecastDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import LocationDetails from "./LocationDetails";
 import getForecast from "../requests/getForecast";
+import SearchForm from "./SearchForm";
 
 const App = () => {
   const [forecasts, setForecasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: "" });
   const [selectedDate, setSelectedDate] = useState(0);
+  const [searchText, setSearchText] = useState("");
 
   const { city, country } = location;
 
@@ -21,13 +23,22 @@ const App = () => {
     setSelectedDate(date);
   };
 
+  const handleCitySearch = () => {
+    getForecast(setSelectedDate, setForecasts, setLocation, searchText);
+  };
+
   useEffect(() => {
-    getForecast(setSelectedDate, setForecasts, setLocation);
+    getForecast(setSelectedDate, setForecasts, setLocation, "");
   }, []);
 
   return (
     <div className="weather-app">
       <LocationDetails city={city} country={country} />
+      <SearchForm
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onSubmit={handleCitySearch}
+      />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
--- src/requests/getForecast.js
+++ src/requests/getForecast.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 
-const getForecast = async (setSelectedDate, setForecasts, setLocation) => {
-  const endpoint =
-    "https://cmd-shift-weather-app.onrender.com/forecast?city=Oxford";
-  const endpointBU = "https://cmd-shift-weather-app-alt.onrender.com/";
+const getForecast = async (
+  setSelectedDate,
+  setForecasts,
+  setLocation,
+  searchText,
+) => {
+  let endpoint = "https://cmd-shift-weather-app.onrender.com/forecast";
+  let endpointBU = "https://cmd-shift-weather-app-alt.onrender.com/forecast";
+
+  if (searchText) {
+    endpoint += `?city=${searchText}`;
+    endpointBU += `?city=${searchText}`;
+  }
   try {
     const { data } = await axios.get(endpoint || endpointBU);
     setSelectedDate(data.forecasts[0].date);
