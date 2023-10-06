128d3067f3022d8886891e554d87317c754dbe31
Tim <50870607+toothless13@users.noreply.github.com>
2023-10-06 12:40:01 +0100

    add error handling tests

--- src/tests/components/LocationDetails.test.js
+++ src/tests/components/LocationDetails.test.js
@@ -1,5 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
 import { render } from "@testing-library/react";
 import LocationDetails from "../../components/LocationDetails";
 
@@ -10,5 +8,37 @@ describe("LocationDetails", () => {
     );
 
     expect(getByText("Manchester, UK")).toBeInstanceOf(HTMLHeadingElement);
+  });
+
+  it("handles 404 errors correctly", () => {
+    const { getByText } = render(
+      <LocationDetails
+        city="Manc"
+        country="UK"
+        errorMessage="No such town or city, please try again!"
+      />,
+    );
+
+    expect(getByText("No such town or city, please try again!")).toBeTruthy();
+    expect(
+      getByText("No such town or city, please try again!"),
+    ).toBeInTheDocument();
+  });
+
+  it("handles 500 errors correctly", () => {
+    const { getByText } = render(
+      <LocationDetails
+        city="Manchester"
+        country="UK"
+        errorMessage="There was a server error, please try again later!"
+      />,
+    );
+
+    expect(
+      getByText("There was a server error, please try again later!"),
+    ).toBeTruthy();
+    expect(
+      getByText("There was a server error, please try again later!"),
+    ).toBeInTheDocument();
   });
 });
